23bf399a0a2fa073255bc4edca2b8a59e147b972
Fatih Ünsever <fatihunseverr@gmail.com>
2017-03-17 18:29:34 +0300

    block: updated row iosched
    Signed-off-by: Fatih Ünsever <fatihunseverr@gmail.com>

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -62,16 +62,16 @@ static const bool queue_idling_enabled[] = {
 static const int queue_quantum[] = {
 	100,	/* ROWQ_PRIO_HIGH_READ */
 	100,	/* ROWQ_PRIO_REG_READ */
-	2,	/* ROWQ_PRIO_HIGH_SWRITE */
-	1,	/* ROWQ_PRIO_REG_SWRITE */
-	1,	/* ROWQ_PRIO_REG_WRITE */
-	1,	/* ROWQ_PRIO_LOW_READ */
-	1	/* ROWQ_PRIO_LOW_SWRITE */
+	4,	/* ROWQ_PRIO_HIGH_SWRITE */
+	2,	/* ROWQ_PRIO_REG_SWRITE */
+	2,	/* ROWQ_PRIO_REG_WRITE */
+	4,	/* ROWQ_PRIO_LOW_READ */
+	2	/* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues */
-#define ROW_IDLE_TIME_MSEC 5	/* msec */
-#define ROW_READ_FREQ_MSEC 20	/* msec */
+#define ROW_IDLE_TIME_MSEC 10	/* msec */
+#define ROW_READ_FREQ_MSEC 50	/* msec */
 
 /**
  * struct rowq_idling_data -  parameters for idling on the queue
@@ -154,7 +154,7 @@ struct row_data {
 	unsigned int			cycle_flags;
 };
 
-#define RQ_ROWQ(rq) ((struct row_queue *) ((rq)->elevator_private[0])) 
+#define RQ_ROWQ(rq) ((struct row_queue *) ((rq)->elv.priv[0]))
 
 #define row_log(q, fmt, args...)   \
 	blk_add_trace_msg(q, "%s():" fmt , __func__, ##args)
@@ -553,7 +553,7 @@ row_set_request(struct request_queue *q, struct request *rq, gfp_t gfp_mask)
 	unsigned long flags;
 
 	spin_lock_irqsave(q->queue_lock, flags);
-	rq->elevator_private[0] =
+	rq->elv.priv[0] =
 		(void *)(&rd->row_queues[get_queue_type(rq)]);
 	spin_unlock_irqrestore(q->queue_lock, flags);
 
